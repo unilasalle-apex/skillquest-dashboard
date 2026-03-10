1d0a0979e5d2899fdcabcf7cbfaae58b516cb5cb
Julien Duquennoy <52005031+APEX-UniLaSalle@users.noreply.github.com>
2026-03-10 14:36:39 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -42,7 +42,8 @@ FILE_NAMES = [
     "xp_SkillQuest_2026-02-18.xlsx",
     "xp_SkillQuest_2026-03-02.xlsx",
     "xp_SkillQuest_2026-03-03.xlsx",
-    "xp_SkillQuest_2026-03-06.xlsx"
+    "xp_SkillQuest_2026-03-06.xlsx",
+    "xp_SkillQuest_2026-03-10.xlsx"
 ]
 
 # Constantes pour les niveaux et les couleurs
